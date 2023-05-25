0d7cbf29b1455c0166f318758e4ef8d687f344a0
Marc JOURNO <mjourno@e1r7p19.clusters.42paris.fr>
2023-05-25 11:14:16 +0200

    cpp

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-#include <cctype>
-#include <cstring>
+#include <string>
 
 int	main(int argc, char **argv) {
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 		for (int i = 1; i < argc; i++)
-			for (int j = 0; j < (int)strlen(argv[i]); j++)
-				std::cout << (char)toupper(argv[i][j]);
+		{
+			std::string str = argv[i];
+			for (unsigned long j = 0; j < str.length(); j++)
+				std::cout << (char)std::toupper(str[j]);
+		}
 	std::cout << std::endl;
 	return 0;
 }
